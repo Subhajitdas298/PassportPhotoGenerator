770db4d6fc54e760592c5fd6074048d33f90c27b
Subhajitdas298 <subhajitdas298@live.com>
2018-05-09 13:14:54 +0530

    Added eye detection criteria to filter out false positives

--- face_multi_detect_passport_gen.m
+++ face_multi_detect_passport_gen.m
@@ -26,6 +26,30 @@ FDetect = vision.CascadeObjectDetector( ...
 %Returns Bounding Box values based on number of objects
 BB = step(FDetect,I);
 
+% applying extra filtering for human face detection
+EyeDetect = vision.CascadeObjectDetector('EyePairBig');
+BBEye=step(EyeDetect,I);
+
+BBNew = [[],[]];
+
+s = 1;
+for i = 1:size(BB,1)
+    for j = 1: size(BBEye, 1)
+        if BBEye(j, 1) >= BB(i, 1) && ...
+           BBEye(j, 2) >= BB(i, 2) && ...
+           BBEye(j, 1) + BBEye(j, 3) <= BB(i, 1) + BB(i, 3) && ...
+           BBEye(j, 2) + BBEye(j, 4) <= BB(i, 2) + BB(i, 4)
+       
+           BBNew(s,:) = BB(i, :);
+           s = s + 1;
+           
+           break;
+        end
+    end
+end
+
+BB = BBNew;
+
 %% stop if no face is found
 if isempty(BB)
      disp('No faces detected.');
